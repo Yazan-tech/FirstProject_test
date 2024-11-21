81102d695e787c833a4facca7887827b52335cc5
yazan <yazanayoup@work.com>
2024-11-21 20:11:33 +0300

    add welcome meesage

--- pyhontest.py
+++ pyhontest.py
@@ -9,4 +9,4 @@ print ("let's go and make the first project which is simple calculator")
 # - for substract
 #+ for summation
 # % for reminder
-
+print ("Welcom, Can you choose your operation from the down menu:/ for dividsion ,* for mltible,+ for summation, - for substraction, % for remainder")
