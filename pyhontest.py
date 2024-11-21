8c040b482f349ad192c10b417f637c346ec5bcfb
yazan <yazanayoup@work.com>
2024-11-21 20:01:15 +0300

    this for ensure everything is okay

--- pyhontest.py
+++ pyhontest.py
@@ -1,3 +1,12 @@
 print("This is the first project on git ")
 print ("let's go and make the first project which is simple calculator")
 
+#make the code for calculator
+#let's define the item that we need first
+#result which eqaul result of summation
+#/ for division
+# * for multible
+# - for substract
+#+ for summation
+# % for reminder
+
